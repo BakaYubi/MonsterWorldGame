f89a394270e8dbb0f3af7a6d8906166a9d39088f
BakaYubi <44865616+BakaYubi@users.noreply.github.com>
2018-11-14 09:10:42 +0900

    20181114

---
+++ MonsterWorld.cpp
@@ -0,0 +1,121 @@
+#include "MonsterWorld.h"
+#include "VariousMonster.h"
+
+bool MonsterWorld::isDone()
+{
+	if (countItems() == 0) {
+		cout << "아이템 = 0 --------> 게임 종료 ..... \n\n";
+		return true;
+	}
+	if (nMon == 0) {
+		cout << "몬스터 = 0 --------> 게임 종료 ..... \n\n";
+		return true;
+	}
+	return false;
+}
+
+int MonsterWorld::countItems() {
+	int nItems = 0;
+	for (int y = 0; y < yMax; y++)
+		for (int x = 0; x < xMax; x++)
+			if (Map(x, y) > 0) nItems++;
+	return nItems;
+}
+void MonsterWorld::print() {
+	canvas.clear();
+	for (int y = 0; y < yMax; y++)
+		for (int x = 0; x < xMax; x++)
+			if (Map(x, y) > 0) canvas.draw(x, y, "■");
+	for (int i = 0; i < nMon; i++)
+	//	mon[i].draw(canvas);
+		pMon[i]->draw(canvas);
+	canvas.print("[ Monster World (몬스터들의 세상) ]");
+
+	cerr << " 전체 이동 횟수 = " << nMove << endl;
+	cerr << " 남은 아이템 수 = " << countItems() << endl;
+	for (int i = 0; i < nMon; i++)
+	//	mon[i].print();
+		pMon[i]->print();
+}
+
+MonsterWorld::MonsterWorld(int w, int h, int n) : world(h, w), canvas(w, h), xMax(w), yMax(h), nItemCnt(n) {
+	nMon = 0;
+	nMove = 0;
+
+	for (int y = 0; y < yMax; y++)
+		for (int x = 0; x < xMax; x++) Map(x, y) = 0;
+
+	int x, y, cnt;
+	cnt = nItemCnt;
+	while (cnt) {
+		x = (rand() % xMax);
+		y = (rand() % yMax);
+		if (Map(x, y) == 0) {
+			Map(x, y) = 1;	
+			cnt--;
+		}
+	}
+}
+ 
+//void MonsterWorld::add(Monster& m) {
+void MonsterWorld::add(Monster* m) {
+	//cout << "몬스터 이름 = " << m->getName() << endl;
+	//	if (nMon < MAXMONS) mon[nMon++] = m;
+	if (nMon < MAXMONS) pMon[nMon++] = m;
+}
+#include <assert.h>
+void MonsterWorld::play(int maxwalk, int wait) {
+		print();
+		cerr << " 엔터를 누르세요...";
+		getchar();
+		for (int i = 0; i < maxwalk; i++) {
+//			for (int k = 0; k < nMon; k++)
+	//			mon[k].move(map, xMax, yMax);
+//				pMon[k]->move(world.Data(), xMax, yMax);
+
+			/*
+			((Zombie*)pMon[0])->move(world.Data(), xMax, yMax);   // 형변환이 필요함
+			((Vampire*)pMon[1])->move(world.Data(), xMax, yMax);
+			((KGhost*)pMon[2])->move(world.Data(), xMax, yMax);
+			((Smombi*)pMon[3])->move(world.Data(), xMax, yMax);
+			((SuperSmombi*)pMon[4])->move(world.Data(), xMax, yMax);
+			((Jiangshi*)pMon[5])->move(world.Data(), xMax, yMax);
+			((Jiangshi*)pMon[6])->move(world.Data(), xMax, yMax);
+			*/
+
+			for (size_t i = 0; i < nMon; i++)
+			{
+				pMon[i]->move(world.Data(), xMax, yMax);
+
+			}
+
+
+			for (size_t i = 0; i < nMon; i++)
+			{
+				(pMon[i])->move(world.Data(), xMax, yMax);
+			}
+			nMove++;
+			print();
+			checkEnergy();
+			if (isDone()) break;
+			Sleep(wait);
+		}
+		
+}
+
+void MonsterWorld::checkEnergy()
+{
+	for (size_t i = 0; i < nMon; i++)
+	{
+		if (pMon[i]->getEnergy() == 0) {
+			string str = pMon[i]->getName();
+			cout << "\t" << str << " Monster가 굶어 죽었습니다." << endl;
+			delete pMon[i];
+			if (i != (nMon - 1)) {
+				pMon[i] = pMon[nMon - 1];
+			}
+			nMon--;
+			system("pause");
+		}
+	}
+}
---
+++ MonsterWorld.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Canvas.h"
+#include "Monster.h"
+#include "Matrix.h"
+#include <windows.h>
+#define DIM  40
+#define MAXMONS 6
+
+class MonsterWorld {
+//	int map[DIM][DIM];      // MonsterWorld 按眉 积己矫 map[40][40] 皋葛府 且寸
+	Matrix world;
+	int xMax, yMax, nMon, nMove, nItemCnt;
+//	Monster mon[MAXMONS];    // MonsterWorld 按眉 积己矫 Monster按眉 积己
+	Monster* pMon[MAXMONS];
+	Canvas	canvas;          
+
+	int& Map(int x, int y) { return world.elem(x, y); }
+	bool isDone();
+	int countItems();  
+	void print(); 
+public:
+	MonsterWorld(int w, int h, int n);
+	~MonsterWorld() { 
+		for (int i = 0; i < nMon; i++) {
+			delete pMon[i];
+		}
+	}
+	void add(Monster* m);  
+	void play(int maxwalk, int wait);  
+	void checkEnergy();
+};
